f428c551ae4d93f30a191cfb2b7acf568b95a2c1
Даша <ccslls368@gmail.com>
2023-07-25 16:32:39 +0500

    Фиксы, добавление инпута от пользователя

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://learnenglish.britishcouncil.org/'
+url = input("📄 Введите ссылку на статью: ")
 page = requests.get(url)
-print(page.status_code)
 soup = BeautifulSoup(page.text, "html.parser")
-print(soup)
+
+content = []
+filteredContent = []
+
+content = soup.findAll('div', class_='content')
+
+for data in content:
+    if data.find('p') is not None:
+        filteredContent.append(data.text)
+
+for data in filteredContent:
+    print(data)
